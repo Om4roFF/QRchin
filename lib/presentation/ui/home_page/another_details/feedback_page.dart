ed93ef432d941434d1ee48597c08f34a84d01851
Bolat Omarov <bolatomarov2000@gmail.com>
2021-07-24 23:13:10 +0600

    history page, history view, many fixes

--- lib/presentation/ui/home_page/another_details/feedback_page.dart
+++ lib/presentation/ui/home_page/another_details/feedback_page.dart
@@ -246,13 +246,12 @@ class _ThemeOfMessage extends StatelessWidget {
       itemBuilder: (context) {
         final list = List.generate(
           popUpText.length,
-              (index) =>
-              PopupMenuItem(
-                child: SizedBox(
-                  width: 600,
-                  child: Text(popUpText[index]),
-                ),
-              ),
+          (index) => PopupMenuItem(
+            child: SizedBox(
+              width: 600,
+              child: Text(popUpText[index]),
+            ),
+          ),
         );
         return list;
       },
